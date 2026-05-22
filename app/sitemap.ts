0f08f3456cc326b234acdd617d9f5e895a642c71
nikkiliew0814-pixel <nikkiliew0814@gmail.com>
2026-05-22 15:45:41 +0800

    Add sitemap generation for website

---
+++ app/sitemap.ts
@@ -0,0 +1,15 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://pasangunifi.com',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://pasangunifi.com/about',
+      lastModified: new Date(),
+    },
+    // 继续加你的其他页面
+  ]
+}
